fd9045c9fbf0a5fdfe5f98b397fd983b67e49d69
minh <ngm.mq1901@gmail.com>
2024-10-28 18:32:49 +0700

    new

---
+++ Page/learning-path-Front.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>  
+<html lang="en">  
+<head>  
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href="../css/learning-path-Front.css">  
+    <title>Front-end</title>  
+</head>  
+<body>  
+<?php include '../Component/Header.php';?>
+<div class="container" style="padding: 30px;">  
+        <h1>WELCOME TO Front-end</h1>  
+        <p>  
+        Most websites or mobile applications have two parts: Front-end and Back-end. The front-end is the
+         part of the user interface that is visible and interactable. The back-end is where data is processed and stored. 
+         Therefore, the Back-end programmer's task is to analyze and design data and process business logic of functions in the application.        </p>  
+
+        <h2>1. HTML và CSS</h2>  
+        <p>To learn Front-end web, we always start with HTML and CSS, these are two languages ​​present in every website on the internet.</p>
+        <table>  
+            <tr>  
+                <td class="image-cell"><img src="HTML&Css.jpg" alt="HTML & CSS" /></td>  
+                <td class="info">  
+                    <p>HTML và CSS</p>  
+                    <p>From basic to advanced, practice 8 projects, hundreds of exercises, private question and answer page, certificate after the course and buy once to study forever.</p>  
+                    <button>See More</button>  
+                </td>  
+            </tr>  
+        </table>  
+
+        <h2>2. JavaScript</h2>  
+        <p>
+        With HTML and CSS, you can only build static websites, only including the interface and almost no interactive processing.
+         To add rich functions and increase interactivity to your website, you need to learn Javascript.
+        </p>
+        <table>  
+            <tr>  
+                <td class="image-cell"><img src="java.jpg" alt="JavaScript" /></td>  
+                <td class="info">  
+                    <p>JavaScript</p>  
+                    <p>Learning basic Javascript is suitable for people who have never learned programming.
+                         With more than50px lessons and practice exercises after each lesson.</p>  
+                    <button>See More</button>  
+                </td>  
+            </tr>  
+        </table>  
+
+        <h2>3. React JS</h2>  
+        <p>
+        With HTML and CSS, you can only build static websites, only including the interface and almost no interactive processing. 
+        To add rich functions and increase interactivity to your website, you need to learn Javascript.
+        </p>
+        <table>  
+            <tr>  
+                <td class="image-cell"><img src="react js.jpg" alt="React JS" /></td>  
+                <td class="info">  
+                    <p>React JS</p>  
+                    <p>ReactJS course from basic to advanced, the result of this course
+                         is that you can do most common projects with ReactJS. At the end of this course you will have a project</p>  
+                    <button>See More</button>  
+                </td>  
+            </tr>  
+        </table>  
+
+    </div>  
+    <?php include '../Component/Footer.php'; ?>
+</body>  
+</html>
---
+++ demo/News.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail_lecturer</title>
+    <link rel="stylesheet" href="News.css">
+    <link rel="stylesheet" href="../css/Blog.css">
+</head>
+
+<body>
+    <?php include '../Component/Header.php';?>
+    <!-- About -->
+    <div class="content-container">
+        <h3>Xin chào mọi người, mình là Lý Cao Nguyên, mình đã làm một dự án website front-end với hơn 100 bài học và
+            200 bài viết.</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/667548f5c4e1b.png"
+            alt="Image showing project completion" , width="850px" , height="350px" style="border-radius: 10px;">
+
+        <p>Bài viết này sẽ bao gồm các phần như sau:</p>
+        <ol>
+            <li>Tại sao mình lại làm một dự án trong một thời gian ngắn như vậy?</li>
+            <li>Lúc mất dữ liệu mình ra sao?</li>
+            <li>Cách mình đã làm để hoàn thành dự án chỉ trong 15 ngày.</li>
+        </ol>
+
+        <h3>1. Tại sao mình lại làm một dự án trong một thời gian ngắn như vậy?</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/66754a1dd074d.png"
+            alt="Laptop issue and data loss" , width="850px" , height="350px" style="border-radius: 10px;">
+        <p>Lí do mình làm một dự án trong thời gian ngắn như vậy là do một số lỗi trong quá trình xây dựng gần đến ngày
+            dự thi. Laptop của mình bị hỏng (mình không dùng GitHub) nên tất cả code bay theo hư vô khi laptop được sửa.
+            Mình cố khôi phục dữ liệu nhưng không thành công.</p>
+
+        <h3>2. Lúc mất dữ liệu mình ra sao?</h3>
+        <img src="https://i.pinimg.com/736x/06/98/6a/06986a1609bd2fcbd8cb047c789738d0.jpg"
+            alt="Feeling down after data loss" , width="850px" , height="350px" style="border-radius: 10px;">
+        <p>Lúc mất dữ liệu, mình đã sắp hoàn thành dự án rồi (chỉ còn phần tin tức nữa là xong). Mình bị suy sụp và buồn
+            trong 2 ngày. Mình định bỏ cuộc nhưng lý trí không cho, nên mình phải tiếp tục và bắt đầu lại từ đầu.</p>
+
+        <h3>3. Cách mình đã làm để hoàn thành dự án chỉ trong 15 ngày.</h3>
+
+        <h3>3.1. Lên kế hoạch, thiết kế logo, thiết kế layout, chuẩn bị nội dung cho dự án (5 ngày)</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/66754ccda186a.png"
+            alt="Project planning and layout design" , width="850px" , height="350px" style="border-radius: 10px;">
+        <ul>
+            <li>Mình đã lên kế hoạch, phác thảo khung website và ghi chú các mục trên thanh menu.</li>
+            <li>Thiết kế logo bằng Canva.</li>
+            <li>Vẽ layout cho từng trang để làm mẫu code.</li>
+            <li>Chuẩn bị toàn bộ nội dung Ngữ văn 9 trong file Word.</li>
+        </ul>
+
+        <h3>3.2. Code giao diện cho từng trang (2 ngày)</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/66754dba16f9e.png"
+            alt="Coding the interface for pages" , width="850px" , height="350px" style="border-radius: 10px;">
+        <p>Mình bắt đầu code giao diện cho các trang như Trang chủ, Giới thiệu, Học văn, Soạn văn, Tài liệu,...</p>
+
+        <h3>3.3. Đưa nội dung lên website (5 ngày)</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/667550013c361.png"
+            alt="Adding content to the website" , width="850px" , height="350px" style="border-radius: 10px">
+        <p>Mình copy và dán nội dung từ file Word vào giao diện đã tạo. Quá trình này mất 7 ngày do số lượng nội dung
+            khá lớn.</p>
+
+        <h3>3.4. Phát sinh thêm tính năng (1 ngày)</h3>
+        <img src="https://i.pinimg.com/564x/22/5a/42/225a42c8cfafbd20fa8510b5638fd0d8.jpg" alt="Adding new features" ,
+            width="850px" , height="350px" style="border-radius: 10px;">
+        <p>Trong quá trình code, mình đã nảy ra một số ý tưởng mới và thêm chúng vào trang web chỉ trong 1 ngày.</p>
+
+        <h3>3.5. Đưa website lên hosting (1 ngày)</h3>
+        <img src="https://files.fullstack.edu.vn/f8-prod/blog_posts/10849/6675503d89bb6.png" alt="Hosting the website" ,
+            width="850px" , height="350px" style="border-radius: 10px;">
+        <p>Sau khi hoàn thành, mình đã đẩy code lên hosting. Do hosting miễn phí có hạn chế về không gian lưu trữ, mình
+            phải chia thành nhiều kho lưu trữ.</p>
+
+        <h3>3.6. Sửa lỗi sau khi upload code và chạy thử nghiệm (1 ngày)</h3>
+        <img src="https://i.pinimg.com/564x/96/cf/a9/96cfa90d02e6b1d03be915dd71074e79.jpg" alt="Image 8" , width="850px"
+            , height="350px" style="border-radius: 10px;">
+        <p>Trong quá trình thử nghiệm, trang web bị lỗi ở một số chỗ. Sau khi sửa, mình đã chia sẻ trang web lên mạng xã
+            hội.</p>
+        <br><br>
+        <p>Cảm ơn mọi người đã đọc!</p>
+        <hr>
+        <section class="news">
+            <div class="cards-new">
+                <!-- Repeatable Card Structure -->
+                <div class="card">
+                    <img src="news-image1.png" alt="News 1">
+                    <h3>News Title 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <button>Read More</button>
+                </div>
+                <div class="card">
+                    <img src="news-image1.png" alt="News 1">
+                    <h3>News Title 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <button>Read More</button>
+                </div>
+                <div class="card">
+                    <img src="news-image1.png" alt="News 1">
+                    <h3>News Title 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <button>Read More</button>
+                </div>
+        </section>
+    </div>
+    <?php include '../Component/Footer.php';?>
+</body>
